7c4dc1eaab6f6266796393be2b9b00a99613616c
tomekMarkowskiJava <tomekmarkowski92@gmail.com>
2020-05-17 22:15:40 +0200

    Added model for the employees and test home page

---
+++ src/main/java/com/afroalko/employee/model/Employee.java
@@ -0,0 +1,26 @@
+package com.afroalko.employee.model;
+
+public class Employee {
+    private String name;
+    private Integer numberOfVotes;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumberOfVotes() {
+        return numberOfVotes;
+    }
+
+    public void setNumberOfVotes(Integer numberOfVotes) {
+        this.numberOfVotes = numberOfVotes;
+    }
+}
