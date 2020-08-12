0f10cc89f294ce31da351e4cd9c1d5f2d4abf687
tomekMarkowskiJava <tomekmarkowski92@gmail.com>
2020-08-12 22:44:24 +0200

    added simple security

--- src/main/java/com/afroalko/employee/model/Employee.java
+++ src/main/java/com/afroalko/employee/model/Employee.java
@@ -1,11 +1,32 @@
 package com.afroalko.employee.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Integer id;
     private String name;
     private Integer numberOfVotes;
 
+    public Employee() {
+    }
+
     public Employee(String name) {
         this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getName() {
---
+++ src/main/java/com/afroalko/employee/model/security/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.afroalko.employee.model.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user1 = User.withDefaultPasswordEncoder()
+                .username("Daria")
+                .password("daria1")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user1);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/voting").permitAll()
+                .anyRequest().hasRole("ADMIN")
+                .and()
+                .formLogin().permitAll();
+    }
+}
