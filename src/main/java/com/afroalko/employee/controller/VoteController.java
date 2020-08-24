e01716877a6956e8b891ba9696fa9b4dbb24844e
tomekMarkowskiJava <tomekmarkowski92@gmail.com>
2020-08-24 17:15:22 +0200

    test2

--- src/main/java/com/afroalko/employee/controller/VoteController.java
+++ src/main/java/com/afroalko/employee/controller/VoteController.java
@@ -2,6 +2,7 @@ package com.afroalko.employee.controller;
 
 import com.afroalko.employee.manager.EmployeeManager;
 import com.afroalko.employee.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,15 @@ public class VoteController {
 
     private EmployeeManager employeeManager;
 
-    public VoteController() {
+    @Autowired
+    public VoteController(EmployeeManager employeeManager) {
+        this.employeeManager = employeeManager;
+        employeeManager.add(new Employee("Tomek"));
+        employeeManager.add(new Employee("Julia"));
     }
 
     @GetMapping("/vote")
     public Iterable<Employee> showEmployees(){
-        employeeManager.add(new Employee("Tomek"));
-        employeeManager.add(new Employee("Julia"));
         return employeeManager.findAll();
     }
 }
