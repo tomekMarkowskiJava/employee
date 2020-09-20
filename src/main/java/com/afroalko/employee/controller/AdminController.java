518769200c01051ad35e761414be3891ecda00db
tomekMarkowskiJava <tomekmarkowski92@gmail.com>
2020-09-20 20:47:42 +0200

    Add admin functions in "/admin" page Refactor

---
+++ src/main/java/com/afroalko/employee/controller/AdminController.java
@@ -0,0 +1,37 @@
+package com.afroalko.employee.controller;
+
+import com.afroalko.employee.manager.EmployeeManager;
+import com.afroalko.employee.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private EmployeeManager employeeManager;
+
+    @Autowired
+    public AdminController(EmployeeManager employeeManager) {
+        this.employeeManager = employeeManager;
+    }
+
+    @GetMapping
+    public String adminMenu(Model model) {
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("employeeList", employeeManager.findAll());
+        return "admin";
+    }
+
+    @PostMapping
+    public String addEmployee(@ModelAttribute("employee") Employee employee, Model model) {
+        employeeManager.add(employee);
+        model.addAttribute("employeeList", employeeManager.findAll());
+        return "redirect:/admin";
+    }
+}
--- src/main/java/com/afroalko/employee/controller/VoteController.java
+++ src/main/java/com/afroalko/employee/controller/VoteController.java
@@ -1,12 +1,10 @@
 package com.afroalko.employee.controller;
 
 import com.afroalko.employee.manager.EmployeeManager;
-import com.afroalko.employee.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class VoteController {
@@ -16,9 +14,6 @@ public class VoteController {
     @Autowired
     public VoteController(EmployeeManager employeeManager) {
         this.employeeManager = employeeManager;
-        employeeManager.clear();
-        employeeManager.add(new Employee("Tomek", "admin", "admin"));
-        employeeManager.add(new Employee("Julia", "user", "user"));
     }
 
     @GetMapping("/vote")
