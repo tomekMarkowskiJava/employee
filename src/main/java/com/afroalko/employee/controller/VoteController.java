d750fc0ee86a41754c49acc521e6c41bedd236a7
tomekMarkowskiJava <tomekmarkowski92@gmail.com>
2020-09-25 13:44:35 +0200

    delete clearing employee list after starting app

--- src/main/java/com/afroalko/employee/controller/VoteController.java
+++ src/main/java/com/afroalko/employee/controller/VoteController.java
@@ -1,7 +1,6 @@
 package com.afroalko.employee.controller;
 
 import com.afroalko.employee.manager.EmployeeManager;
-import com.afroalko.employee.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,8 +14,6 @@ public class VoteController {
     @Autowired
     public VoteController(EmployeeManager employeeManager) {
         this.employeeManager = employeeManager;
-        employeeManager.clear();
-        employeeManager.add(new Employee("Tomek","admin","admin"));
     }
 
     @GetMapping("/vote")
